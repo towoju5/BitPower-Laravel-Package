5293248d6e83e0508077f912b038c4ecc90028ee
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2022-11-17 16:30:04 +0100

    update

--- src/Bitpowr.php
+++ src/Bitpowr.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @author Towoju5
+ * @author url https://towoju.com.ng
+ * @orginazation Woju Technologies
+ */
+
 
 namespace Towoju5\Bitpowr;
 
@@ -6,36 +12,51 @@ class Bitpowr
 {
     public function __construct()
     {
-        $this->baseUrl = "https://developers.bitpowr.com/api/v1";
+        $this->baseUrl = "https://developers.bitpowr.com/api/v1/";
     }
 
-    public function createWhitelistAddress()
+    /**
+     * This endpoints allows you to create a whitelist address
+     * @param array accountId =>   "50ec9b81-ad6c-4db2-98b8-e88025ceabf8"
+     */
+    public function createWhitelistAddress($data)
     {
-        //
+        return $this->run_curl("addresses/whitelist", "POST", $data);
     }
 
-    public function createAddress()
+    /**
+     * @param array data [
+     *      @param string label
+     *      @param string asset
+     *      @param string accountId
+     *      @param string addressType
+     * ]
+     */
+    public function createAddress(array $data)
     {
-        //
+        return $this->run_curl("addresses", "POST", $data);
     }
 
-    public function getAddress()
+    /**
+     * This endpoint generates a list of all created address for a sub account
+     */
+    public function getAddress($data)
     {
-        //
+        return $this->run_curl("addresses", "GET", $data);
     }
 
-    public function getAddressById()
+    public function getAddressById($data)
     {
-        //
+        return $this->run_curl("addresses", "GET", $data);
     }
 
     public function getAddressTransactions()
     {
-        //
+        return $this->run_curl("addresses", "GET", $data);
     }
 
     public function getAddressBalance()
     {
-        //
+        return $this->run_curl("addresses", "GET", $data);
     }
 }
