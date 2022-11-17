3995ec998daa1f640eb0c713175f653f1c3c2b4f
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2022-11-17 16:05:40 +0100

    methods created

--- src/Bitpowr.php
+++ src/Bitpowr.php
@@ -4,5 +4,38 @@ namespace Towoju5\Bitpowr;
 
 class Bitpowr
 {
-    // Build your next great package.
+    public function __construct()
+    {
+        $this->baseUrl = "https://developers.bitpowr.com/api/v1";
+    }
+
+    public function createWhitelistAddress()
+    {
+        //
+    }
+
+    public function createAddress()
+    {
+        //
+    }
+
+    public function getAddress()
+    {
+        //
+    }
+
+    public function getAddressById()
+    {
+        //
+    }
+
+    public function getAddressTransactions()
+    {
+        //
+    }
+
+    public function getAddressBalance()
+    {
+        //
+    }
 }
